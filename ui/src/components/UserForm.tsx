fcbe9ce84ac6b150aa2355a871a269ca7603bb5e
VladPodilnyk <vladyslav.podilnyk@bolt.eu>
2024-08-03 13:52:27 +0300

    minor ui adjustment.

--- ui/src/components/UserForm.tsx
+++ ui/src/components/UserForm.tsx
@@ -61,6 +61,7 @@ export function UserForm() {
                     value={alias}
                     error={rq.isAliasInvalid}
                     onChange={onAliasChange}
+                    size='compact'
                   />
                 </FormControl>
               </Block>
